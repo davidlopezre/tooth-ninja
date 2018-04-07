6e4229c7923aa9803a13c6b11dcaaa9b7b9e441a
David Lopez <dlopez1@student.unimelb.edu.au>
2018-04-08 02:15:42 +1000

    Bacteria can approach the tooth and collisions are detected. Note: Have not attempted to make this work with more than one tooth and spawn points for the bacteria can be better. Also, will improve the frequency of my commits :)

---
+++ tooth-ninja/tooth-ninja/GameViewController.swift
@@ -0,0 +1,30 @@
+//
+//  GameViewController.swift
+//  tooth-ninja
+//
+//  Created by David Lopez on 7/4/18.
+//  Copyright © 2018 David Lopez. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+
+class GameViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let scene = GameScene(size: view.bounds.size)
+        let skView = view as! SKView
+        skView.showsFPS = true
+        skView.showsNodeCount = true
+        skView.ignoresSiblingOrder = true
+        scene.scaleMode = .resizeFill
+        skView.presentScene(scene)
+    }
+    
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+    
+}
+
