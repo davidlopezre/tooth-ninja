b4f6fa448097e62464f8ad876275393399b57205
Kushagra Vashisht <kvashisht@student.unimelb.edu.au>
2018-07-27 01:20:40 +1000

    Some changes, not permanent

---
+++ tooth-ninja/tooth-ninja/Controller/SwipingController+UICollectionView.swift
@@ -0,0 +1,33 @@
+//
+//  SwipingController+UICollectionView.swift
+//  autolayout_lbta
+//
+//  Created by Brian Voong on 10/27/17.
+//  Copyright © 2017 Lets Build That App. All rights reserved.
+//
+
+import UIKit
+
+extension SwipingController {
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 0
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return pages.count
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cellId", for: indexPath) as! PageCell
+        
+        let page = pages[indexPath.item]
+        cell.page = page
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: view.frame.width, height: view.frame.height)
+    }
+    
+}
---
+++ tooth-ninja/tooth-ninja/Page/Page.swift
@@ -0,0 +1,19 @@
+//
+//  Page.swift
+//  autolayout_lbta
+//
+//  Created by Brian Voong on 10/13/17.
+//  Copyright © 2017 Lets Build That App. All rights reserved.
+//
+
+import Foundation
+
+struct Page {
+    
+    let imageName: String
+    let headerText: String
+    let bodyText: String
+    
+}
+
+
