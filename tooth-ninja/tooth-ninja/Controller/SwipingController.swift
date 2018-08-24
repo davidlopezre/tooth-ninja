d8029aad09b424c5505005416c7fd95658e42fb3
Kushagra Vashisht <kvashisht@student.unimelb.edu.au>
2018-08-25 01:53:58 +1000

    Fixed the errors

--- tooth-ninja/tooth-ninja/Controller/SwipingController.swift
+++ tooth-ninja/tooth-ninja/Controller/SwipingController.swift
@@ -8,6 +8,12 @@
 
 import UIKit
 
+extension UIColor
+{
+    static var mainPink = UIColor(red: 232/255, green: 68/255, blue: 133/255, alpha: 1)
+}
+
+
 class SwipingController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
     
     let pages = [
