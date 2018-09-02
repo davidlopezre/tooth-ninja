1eb9970da0b1e5f8e9cc6e9d9333b9aa2b516c9e
Kushagra Vashisht <kvashisht@student.unimelb.edu.au>
2018-09-02 14:11:07 +1000

    Working on integrating the drop downs

--- tooth-ninja/tooth-ninja/Controller/GameViewController.swift
+++ tooth-ninja/tooth-ninja/Controller/GameViewController.swift
@@ -33,7 +33,6 @@ class GameViewController: UIViewController, Controller
         
         // ADDING THE HIDDEN VIEW CONTROLS FOR THE LONG PRESS GESTURE RECOGNISER HERE
         let longPressGestRecg = UILongPressGestureRecognizer(target: self, action: #selector(openMenu(press:)))
-        //navigationController?.isNavigationBarHidden = true
         longPressGestRecg.minimumPressDuration = 3.0
         
         hiddenView.addGestureRecognizer(longPressGestRecg)
@@ -143,7 +142,7 @@ class GameViewController: UIViewController, Controller
             let storyBoard: UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
             let hiddenViewController = storyBoard.instantiateViewController(withIdentifier: "hiddenViewController")
             self.present(hiddenViewController, animated: true, completion: nil)
-            //hiddenViewController.isNavigationBarHidden = true
+            hiddenViewController.navigationController?.isNavigationBarHidden = false
         }
     }
 }
